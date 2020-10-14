1b653a2f76b272a6d782f7e048808eef55330cbc
SugarPlz <58174736+SugarPlz@users.noreply.github.com>
2020-10-14 14:16:47 +0800

    Add files via upload

---
+++ week3/3-4.c
@@ -0,0 +1,15 @@
+#include"ch03.h"
+int main()
+{
+	FILE *fp;
+	char buf[80];
+	memset(buf,0,sizeof(buf));
+	if( (fp=fopen("test1.txt","w"))==NULL )
+		perror("open failed\n");
+	fgets(buf,sizeof(buf),stdin);
+	printf("Content is %s\n",buf);
+	fwrite(buf,1,sizeof(buf),fp);
+	fclose(fp);
+	return 0;
+}
+	
---
+++ week3/3-5.c
@@ -0,0 +1,19 @@
+#include"ch03.h"
+int main()
+{
+	FILE *fp;
+	char buf[80];
+	int ret;
+	memset(buf,0,sizeof(buf));
+	if( (fp=fopen("test1.txt","w"))==NULL )
+		perror("open failed\n");
+	printf("Please input strings you want to store in file:\n");
+	
+	fgets(buf,sizeof(buf),stdin);
+	printf("Content is %s.The size of stream is %ld bytes.\n",buf ,sizeof(buf));
+	ret=fwrite(buf,4,sizeof(buf)/4,fp);
+	printf("Actual writing number of item is %d\n",ret);
+	fclose(fp);
+	return 0;
+}
+	
